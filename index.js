007b2b8ecac2f27a617069fe44c82034665a28c2
Giovanni Zarrillo <gazarrillo1105@gmail.com>
2022-06-16 15:25:14 -0400

    added firebase rtdb to project

--- index.js
+++ index.js
@@ -1,11 +1,44 @@
 const { Template } = require("@walletpass/pass-js");
 const fs = require('fs');
 const crypto = require('crypto');
+const prompt = require('prompt-sync')();
 
-async function generatePass() {
+// firebase
+const initializeApp = require("firebase/app").initializeApp;
+const getDatabase = require("firebase/database").getDatabase;
+const ref = require("firebase/database").ref;
+const get = require("firebase/database").get;
+const set = require("firebase/database").set;
+const child = require("firebase/database").child;
+const firebaseConfig = require("./firebaseConfig.json");
+
+const app = initializeApp(firebaseConfig);
+const db = getDatabase(app);
+
+const eventID = prompt("Enter event ID: ");
+
+// get event data from database
+get(child(ref(db), "events/" + eventID)).then((snapshot) => {
+	if (snapshot.exists()) {
+		for (attendee in snapshot.val()) {
+			console.log(snapshot.val()[attendee]);
+			// generates a serial number for each attendee
+			serial = crypto.randomUUID()
+			// update database with serial number and checked in status
+			set(ref(db, "events/" + eventID + "/" + attendee + "/serial"), serial);
+			set(ref(db, "events/" + eventID + "/" + attendee + "/checkedIn"), false);
+			//generate pass for each attendee
+			generatePass(snapshot.val()[attendee].firstName, snapshot.val()[attendee].lastName, snapshot.val()[attendee].id, serial);
+		}
+	} else {
+		console.log("Event not found");
+	}
+});
+
+
+
+async function generatePass(firstName, lastName, id, serial) {
 	
-	console.log("Generating pass...");
-
 	// creates template
 	const template = await Template.load("./template");
 	await template.loadCertificate("certs/certificate.pem", '110500gz');
@@ -13,29 +46,28 @@ async function generatePass() {
 	
 	// creates pass from template
 	pass = template.createPass({
-		serialNumber: crypto.randomUUID()
+		serialNumber: serial
 	});
 
 	// customize pass
 	pass.primaryFields.add({
         "key": "name",
-        "value": "Giovanni Zarrillo",
+        "value": firstName + lastName,
         "label": "Name"
     });
-	pass.primaryFields.add({
-        "key": "id",
-        "value": "27693155",
-        "label": "ID"
-    });
+	
+	if (id != null) {
+		pass.primaryFields.add({
+			"key": "id",
+			"value": id,
+			"label": "ID"
+		});	
+	}
 
 	// add qr code
 	pass.barcodes = [{"message": pass.serialNumber, "format": "PKBarcodeFormatAztec", messageEncoding: "iso-8859-1"}];
 		
 	// save pass to file
 	const buf = await pass.asBuffer();
-	fs.writeFile("./passes/pass.pkpass", buf, () => { });
-	
-	console.log("Pass generated! Serial Number: " + pass.serialNumber);
+	fs.writeFile("./passes/" + serial + ".pkpass", buf, () => { });
 }
-
-generatePass();
