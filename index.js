ba705cfb756427e365ad497ac3d09e6166c41b36
Giovanni Zarrillo <gazarrillo1105@gmail.com>
2022-06-22 13:43:57 -0400

    added firebase storage to upload passes

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const { Template } = require("@walletpass/pass-js");
-const fs = require('fs');
-const crypto = require('crypto');
-const prompt = require('prompt-sync')();
+const fs = require("fs");
+const crypto = require("crypto");
+const prompt = require("prompt-sync")();
 
 // firebase
 const initializeApp = require("firebase/app").initializeApp;
 const getDatabase = require("firebase/database").getDatabase;
+const getStorage = require("firebase/storage").getStorage;
+const sref = require("firebase/storage").ref;
+const uploadBytes = require("firebase/storage").uploadBytes;
 const ref = require("firebase/database").ref;
 const get = require("firebase/database").get;
 const set = require("firebase/database").set;
@@ -14,60 +17,74 @@ const firebaseConfig = require("./firebaseConfig.json");
 
 const app = initializeApp(firebaseConfig);
 const db = getDatabase(app);
+const storage = getStorage(app);
 
 const eventID = prompt("Enter event ID: ");
 
 // get event data from database
 get(child(ref(db), "events/" + eventID)).then((snapshot) => {
-	if (snapshot.exists()) {
-		for (attendee in snapshot.val()) {
-			console.log(snapshot.val()[attendee]);
-			// generates a serial number for each attendee
-			serial = crypto.randomUUID()
-			// update database with serial number and checked in status
-			set(ref(db, "events/" + eventID + "/" + attendee + "/serial"), serial);
-			set(ref(db, "events/" + eventID + "/" + attendee + "/checkedIn"), false);
-			//generate pass for each attendee
-			generatePass(snapshot.val()[attendee].firstName, snapshot.val()[attendee].lastName, snapshot.val()[attendee].id, serial);
-		}
-	} else {
-		console.log("Event not found");
-	}
+  if (snapshot.exists()) {
+    for (attendee in snapshot.val()) {
+      // generates a serial number for each attendee
+      serial = crypto.randomUUID();
+      // update database with serial number and checked in status
+      set(ref(db, "events/" + eventID + "/" + attendee + "/serial"), serial);
+      set(ref(db, "events/" + eventID + "/" + attendee + "/checkedIn"), false);
+      //generate pass for each attendee
+      generatePass(
+        snapshot.val()[attendee].firstName,
+        snapshot.val()[attendee].lastName,
+        snapshot.val()[attendee].id,
+        serial
+      );
+    }
+  } else {
+    console.log("Event not found");
+  }
 });
 
-
-
 async function generatePass(firstName, lastName, id, serial) {
-	
-	// creates template
-	const template = await Template.load("./template");
-	await template.loadCertificate("certs/certificate.pem", '110500gz');
-	await template.images.load("./images");
-	
-	// creates pass from template
-	pass = template.createPass({
-		serialNumber: serial
-	});
+  // creates template
+  const template = await Template.load("./template");
+  await template.loadCertificate("certs/certificate.pem", "110500gz");
+  await template.images.load("./images");
 
-	// customize pass
-	pass.primaryFields.add({
-        "key": "name",
-        "value": firstName + lastName,
-        "label": "Name"
+  // creates pass from template
+  pass = template.createPass({
+    serialNumber: serial,
+  });
+
+  // customize pass
+  pass.primaryFields.add({
+    key: "name",
+    value: firstName + " " + lastName,
+    label: "Name",
+  });
+
+  if (id != null) {
+    pass.primaryFields.add({
+      key: "id",
+      value: id,
+      label: "ID",
     });
-	
-	if (id != null) {
-		pass.primaryFields.add({
-			"key": "id",
-			"value": id,
-			"label": "ID"
-		});	
-	}
+  }
 
-	// add qr code
-	pass.barcodes = [{"message": pass.serialNumber, "format": "PKBarcodeFormatAztec", messageEncoding: "iso-8859-1"}];
-		
-	// save pass to file
-	const buf = await pass.asBuffer();
-	fs.writeFile("./passes/" + serial + ".pkpass", buf, () => { });
+  // add qr code
+  pass.barcodes = [
+    {
+      message: pass.serialNumber,
+      format: "PKBarcodeFormatAztec",
+      messageEncoding: "iso-8859-1",
+    },
+  ];
+
+  // save pass
+  const buf = await pass.asBuffer();
+
+  // upload passes to firebase storage
+  uploadBytes(sref(storage, "events/" + eventID + "/" + serial + ".pkpass"), buf).then(
+    () => {
+      console.log("Pass: " + serial + " uploaded!");
+    }
+  );
 }
